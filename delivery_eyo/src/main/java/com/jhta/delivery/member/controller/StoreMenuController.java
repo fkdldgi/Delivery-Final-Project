7e0afc6d622cc6e3aeee45b6265af6880ac5aa88
test01 <pochis856@naver.com>
2020-07-15 18:10:03 +0900

    상훈 0715 커밋

---
+++ delivery_eyo/src/main/java/com/jhta/delivery/member/controller/StoreMenuController.java
@@ -0,0 +1,37 @@
+package com.jhta.delivery.member.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.jhta.delivery.member.service.StoreService;
+import com.jhta.delivery.member.vo.MenuOptionVo;
+import com.jhta.delivery.member.vo.MenuVo;
+
+@Controller
+public class StoreMenuController {
+	@Autowired
+	private StoreService sservice;
+	
+	//특정 메뉴의 옵션메뉴를 받아서 모달창에 보내주기
+	@RequestMapping(value="/store/menu_option",produces="application/json;charset=UTF-8")
+	@ResponseBody
+	public String option_menu(@RequestBody String snum){
+		int num=Integer.parseInt(snum);
+		List<MenuOptionVo> optionList = sservice.optionInfo(num);
+		MenuVo menuInfo = sservice.menuInfo(num);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("menuInfo",menuInfo);
+		map.put("optionList", optionList);
+		Gson gson=new Gson();
+		String jsonString=gson.toJson(map);
+		System.out.println("jsonString : " + jsonString);
+		return jsonString;
+	}
+}
